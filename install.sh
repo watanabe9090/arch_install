356f9a4bc13cab01285cf2febe2c8bc13939e96d
Yuri Watanabe <yuriwatanabe9090@gmail.com>
2023-04-01 23:00:18 -0300

    Installing gnome

--- install.sh
+++ install.sh
@@ -1,43 +1,112 @@
 #! /bin/bash
 readonly device=/dev/sda
+readonly isEFI=false
 
 
-parted --script --align optimal "$device" \
-mklabel gpt \
-mkpart primary ext4 1MiB 512MiB \
-mkpart primary linux-swap 512MiB 4512MiB \
-mkpart primary ext4 4512MiB 25% \
-mkpart primary ext4 25% 100% 
-# name 1 'ESP' \
-# name 2 'Swap' \
-# name 3 'Root' \
-# name 4 'Home' 
-
-mkfs.fat -F 32 "${device}1"
-mkfs.ext4 -F "${device}3"
-mkfs.ext4 -F "${device}4"
-
-mkswap "${device}2"
-swapon "${device}2"
+log_info() {
+    local now=$(date +"%H:%M:%S.%3N")
+    echo "$now ::> $1"
+}
+log_err() {
+    local now=$(date +"%H:%M:%S.%3N")
+    echo "$now ::> $1"
+}
+OUT="/mnt/cmdsout.out"
 
 
-mkdir -p /mnt/boot/efi
-mkdir /mnt/etc
+DISK_SIZE=$(blockdev --getsize64 "$device")
+DISK_SIZE_GB=$(bc <<< $DISK_SIZE/1073741824)
 
-mount "${device}3" /mnt
-mount "${device}4" /mnt/home
-mount "${device}1" /mnt/boot/efi
+if [ $DISK_SIZE_GB -lt 20 ]; then
+    echo "Disk should be at least 20Gb"
+fi
 
-pacman-key --init
-pacman-key --populate archlinux
-pacman -Sc
-pacstrap -K /mnt base 
+if [[ $ROOT_SIZE -gt 50 ]]; then
+    ROOT_SIZE=$(bc <<< 1024*30)
+else 
+    ROOT_SIZE=$(bc <<< $DISK_SIZE/2097152)
+fi
 
-genfstab -U /mnt >> /mnt/etc/fstab
+
+if [ isEFI = true ]; then
+echo ""
+    # parted --script --align optimal "$device" \
+    # mklabel msdos \
+    # mkpart primary ext4 1MiB 512MiB \
+    # mkpart primary linux-swap 512MiB 4512MiB \
+    # mkpart primary ext4 4512MiB 40% \
+    # mkpart primary ext4 40% 100% &> $OUT 
+
+    # mkfs.fat -F 32 "${device}1" &> $OUT
+    # mkfs.ext4 -F "${device}3" &> $OUT
+    # mkfs.ext4 -F "${device}4" &> $OUT
+
+    # mkdir /mnt/home 
+    # mkdir /mnt/boot
+    # mkdir /mnt/boot/efi
+
+    # mount "${device}1" /mnt/boot/efi
+    # mount "${device}3" /mnt
+    # mount "${device}4" /mnt/home
+else
+    ROOT_END=$(bc <<< $ROOT_SIZE+1024)
+    log_info "Partitioning $device ..."
+    parted --script --align optimal "$device" \
+    mklabel msdos \
+    mkpart primary linux-swap 1MiB 1024MiB \
+    mkpart primary ext4 1024MiB "${ROOT_SIZE}MiB" \
+    mkpart primary ext4 "${ROOT_END}MiB" 100% &> $OUT
+    if [ $? -ne 0 ];then 
+        log_err "Was not possible to parted $device"
+        exit 1
+    fi
+    log_info "$device was successfully partitioned"
+
+    log_info "Creating ext4 file system for ${device}2 ..."
+    if ! mkfs.ext4 -F "${device}2" &> $OUT; then
+        log_err "Failed on ext4 creating for ${device}2"
+    fi
+    log_info "ext4 file system was successfully created for ${device}2 ..."
+
+
+    log_info "Creating ext4 file system for ${device}3 ..."
+    if ! mkfs.ext4 -F "${device}3" &> $OUT; then 
+        log_err "Failed on ext4 creating for ${device}3"
+    fi
+    log_info "ext4 file system was successfully created for ${device}3 ..."
+
+    log_info "Creating swap file system for ${device}1 ..."
+    if ! mkswap "${device}1" &> $OUT; then
+        log_err "Not possible to create swap on ${device}1"
+    fi
+    log_info "swap file system was successfully created for ${device}1"
+
+    log_info "Activating swap on ${device}1 ..."
+    if ! swapon "${device}1" &> $OUT; then
+        log_err "Not possible to activate swap on ${device}1"
+    fi
+    log_info "swap was successfully activated on ${device}1"
+
+    log_info "Mounting ${device}2 on /mnt"
+    mount "${device}2" /mnt &> $OUT
+    mkdir /mnt/home  &> $OUT
+    log_info "Mounting ${device}3 on /mnt/home"
+    mount "${device}3" /mnt/home &> $OUT
+fi
+
+# pacman-key --init
+# pacman-key --populate archlinux
+# pacman -Sc --noconfirm
+# pacman -Sy --noconfirm
+
+log_info "Installing base package..."
+pacstrap -K /mnt base &> $OUT
+
+log_info "Generating fstab..."
+genfstab -U /mnt >> /mnt/etc/fstab &> $OUT
 
 cp /media/sf_MyArch/install_chroot.sh /mnt
-
 arch-chroot /mnt ./install_chroot.sh
 
-umount -R /mnt
-reboot
+# umount -R /mnt
+# reboot
--- install_chroot.sh
+++ install_chroot.sh
@@ -1,30 +1,72 @@
-echo "helloWorld"
+readonly isEFI=false
 
-pacman -Syu --noconfirm reflector 
+log_info() {
+    local now=$(date +"%H:%M:%S.%3N")
+    echo "$now ::> $1"
+}
+log_err() {
+    local now=$(date +"%H:%M:%S.%3N")
+    echo "$now ::> $1"
+}
 
-reflector -l 10 --country 'Brazil' --sort rate --save /etc/pacman.d/mirrorlist
+OUT="/cmdsout.out"
 
-pacman -S --noconfirm linux linux-lts linux-firmware base-devel  
-pacman -S --noconfirm vim git wget curl network-manager grub efibootmgr os-prober
+log_info "Configuring mirror for Brazil"
+pacman -Syu --noconfirm reflector &> $OUT 
+reflector -l 10 --country 'Brazil' --sort rate --save /etc/pacman.d/mirrorlist &> $OUT 
 
-ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
-hwclock --systohc
+log_info "Installing linux and linux-lts and firmwares"
+pacman -S --noconfirm linux linux-lts linux-headers linux-lts-headers linux-firmware &> $OUT   
 
-echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
-echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
-echo "yuri" > /etc/hostname
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime &> $OUT 
+hwclock --systohc &> $OUT 
+
+echo "LANG=pt_BR.UTF-8" > /etc/locale.conf &> $OUT 
+echo "KEYMAP=br-abnt2" > /etc/vconsole.conf &> $OUT 
+echo "yuri" > /etc/hostname &> $OUT 
+
+echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen &> $OUT 
+
+mkinitcpio -p linux &> $OUT 
+mkinitcpio -P linux-lts &> $OUT 
 
 
-mkinitcpio -p linux
-# mkinitcpio -P linux-lts
+if [ isEFI = true ]; then
+    pacman -S --noconfirm vim git wget curl network-manager grub efibootmgr os-prober dosfstools
+    mount /dev/sda1 /boot/EFI
+    echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
 
-# echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck
+    # grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+else
+    log_info "Installing grub"
+    pacman -S --noconfirm grub &> $OUT  #grub os-prober dosfstools &> $OUT 
+    if ! grub-install --target=i386-pc /dev/sda &> $OUT; then
+        log_err "Could not install grub in /dev/sda"
+    fi 
+fi
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
+log_info "Generating grub.cfg"
+grub-mkconfig -o /boot/grub/grub.cfg &> $OUT 
+
+log_info "Installing essential packages for a developer :)"
+pacman -S --noconfirm vim sudo git wget curl networkmanager base-devel &> $OUT 
+systemctl enable NetworkManager.service &> $OUT 
+
+yes toor | passwd
+
+log_info "Adding liberty user"
+groupadd ikary &> $OUT
+useradd -G ikary -m -s /bin/bash liberty &> $OUT 
+yes liberty | passwd liberty &> $OUT
+
+echo '%ikary ALL=(ALL:ALL) ALL' | EDITOR='tee -a' visudo &> $OUT
+
+log_info "Installing gnome"
+pacman -S --noconfirm xorg xorg-server gnome &> $OUT 
+systemctl enable gdm.service &> $OUT
+# systemctl start gdm.service
 
 
-
-### after install
-
-#sudo pacman -S polybar
+pacman -S --noconfirm vlc firefox &> $OUT
